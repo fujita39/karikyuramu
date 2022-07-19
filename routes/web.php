d0e1c8d07976aba875859e7b83b155af2a3dd7fa
fujita39 <20t2134b@shinshu-u.ac.jp>
2022-07-20 01:28:58 +0900

    カリキュラム8－2

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/posts', function () {
+    return view('posts/index');
 });
+
+//Route::get('/posts','Postcontroller@index');
